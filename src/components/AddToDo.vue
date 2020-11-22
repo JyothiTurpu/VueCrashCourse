d7a3ebca1ef7cdb78822e6e813c6eb4f0d4cc009
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-11-21 23:39:32 -0500

    Learnt How to make API calls in Vue

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <form @submit="addToDo">
+            <input type="text" v-model="title" name="title" placeholder="Add TODO">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "AddToDo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addToDo(event) {
+            event.preventDefault();
+            const newToDo = {
+                title: this.title,
+                completed: false
+            };
+            this.$emit('add-todo', newToDo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+form {
+    display: flex;
+}
+
+input[type="text"] {
+    flex: 10;
+    padding: 5px;
+}
+
+input[type="submit"] {
+    flex: 2;
+}
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header class="header">
+            <h1>TODO LIST</h1>
+        </header>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #ffffff;
+    text-align: center;
+    padding: 10px;
+}
+
+.header a {
+    color: #ffffff;
+    padding-right: 5px;
+}
+</style>
